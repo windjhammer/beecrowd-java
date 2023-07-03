29644daa20067b45beefe4949b5f62fedb75434b
Breno Fernandes <fernandesopa@gmail.com>
2023-07-03 20:03:25 -0300

    1002

---
+++ bee1002/Main.java
@@ -0,0 +1,24 @@
+package bee1002;
+
+import java.io.IOException;
+import java.util.Scanner;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+         Scanner sc = new Scanner(System.in);
+         double pi = 3.14159;
+         double raio = sc.nextDouble();
+         double area = pi * (raio * raio);
+         System.out.printf("A=%.4f", area);
+         System.out.println();
+         sc.close();
+    }
+ 
+}
