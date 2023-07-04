f4406ae74a4f136d0deddc9a5774b98feabe8f23
Breno Fernandes <fernandesopa@gmail.com>
2023-07-03 21:14:41 -0300

    1007

---
+++ bee1007/Main.java
@@ -0,0 +1,27 @@
+package bee1007;
+
+import java.io.IOException;
+import java.util.Scanner;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        int D = sc.nextInt();
+
+        int diferenca = (A * B - C * D);
+        System.out.printf("DIFERENCA = " + diferenca);
+        System.out.println();
+        sc.close();
+    }
+ 
+}
