a0567a2605425431e68aeeefca697fa6e8a8dbe1
Breno Fernandes <fernandesopa@gmail.com>
2023-07-05 21:21:56 -0300

    1015

---
+++ bee1015/Main.java
@@ -0,0 +1,25 @@
+package bee1015;
+
+import java.io.IOException;
+import java.util.Scanner;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        double x1 = sc.nextDouble();
+        double y1 = sc.nextDouble();
+        double x2 = sc.nextDouble();
+        double y2 = sc.nextDouble();
+        double distancia = Math.sqrt(Math.pow(x2 - x1, 2)+Math.pow(y2 - y1, 2));
+        System.out.printf("%.4f\n",distancia);
+        sc.close();
+    }
+ 
+}
