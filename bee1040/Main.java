bba9943ea5d3bb15626434f8926efa1c7c5564c8
Breno Fernandes <fernandesopa@gmail.com>
2023-07-20 21:41:39 -0300

    bee1040 aceito

--- bee1040/Main.java
+++ bee1040/Main.java
@@ -1,6 +1,7 @@
 package bee1040;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 /**
@@ -13,22 +14,23 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("0.0");
         double N1 = sc.nextDouble();
         double N2 = sc.nextDouble();
         double N3 = sc.nextDouble();
         double N4 = sc.nextDouble();
         double media = ((N1 * 2.0) + (N2 * 3.0) + (N3 * 4.0) + (N4 * 1.0)) / 10;
-        System.out.printf("Media: %.1f\n", media);
+        System.out.println("Media: " + df.format(media));
         if (media >= 7.0) {
             System.out.println("Aluno aprovado.");
         } else if (media >= 5.0 && media <= 6.9) {
             System.out.println("Aluno em exame.");
             double notaExame = sc.nextDouble();
             media = (media + notaExame) / 2;
-            System.out.printf("Nota do exame: %.1f\n", notaExame);
+            System.out.println("Nota do exame: " + df.format(notaExame));
             if (media >= 5) {
                 System.out.println("Aluno aprovado.");
-                System.out.printf("Media final: %.1f\n", media);
+                System.out.println("Media final: " + df.format(media));
             } else {
                 System.out.println("Aluno reprovado.");
             }
