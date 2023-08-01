681564333c07fa3947af31e4bab45c7e1de9834d
Breno Fernandes <fernandesopa@gmail.com>
2023-08-01 14:07:10 -0300

    bee1044

--- bee1044/Main.java
+++ bee1044/Main.java
@@ -2,25 +2,41 @@ package bee1044;
 
 import java.io.IOException;
 import java.util.Scanner;
-
+ 
 /**
- * IMPORTANT:
- * O nome da classe deve ser "Main" para que a sua solução execute
- * Class name must be "Main" for your solution to execute
- * El nombre de la clase debe ser "Main" para que su solución ejecutar
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
  */
 public class Main {
-
+ 
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        if (A % B == 0 || B % A == 0) {
-            System.out.println("Sao Multiplos");
-        } else {
-            System.out.println("Nao sao Multiplos");
+        double A = sc.nextDouble();
+        double B = sc.nextDouble();
+        double C = sc.nextDouble();
+        double i;
+        if(B > A && B > C){
+            i = A;
+            A = B;
+            B = i;
+        }
+        else if(C >A){
+            i = A;
+            A = C;
+            C = i;
+        }
+        if (A >= (B + C)) { 
+			System.out.println("NAO FORMA TRIANGULO");
+		} else {
+			if (A*A == (B*B) + (C*C)) System.out.println("TRIANGULO RETANGULO");
+			if (A*A > (B*B) + (C*C)) System.out.println("TRIANGULO OBTUSANGULO");
+			if (A*A < (B*B) + (C*C)) System.out.println("TRIANGULO ACUTANGULO");
+			if (A == B && A == C) System.out.println("TRIANGULO EQUILATERO");
+			if ((A == B && A != C) || (A == C && A != B) || (B == C && B != A)) System.out.println("TRIANGULO ISOSCELES");
         }
         sc.close();
     }
-
+ 
 }
