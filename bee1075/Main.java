92b76a86fbb113d01c2fc63af7ca1fa632595903
Breno Fernandes <fernandesopa@gmail.com>
2023-10-10 10:28:53 -0300

    bee1075

---
+++ bee1075/Main.java
@@ -0,0 +1,25 @@
+package bee1075;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * IMPORTANT:
+ * O nome da classe deve ser "Main" para que a sua solução execute
+ * Class name must be "Main" for your solution to execute
+ * El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i = 1; i < 10000; i++) {
+            if (i % N == 2) {
+                System.out.println(i);
+            }
+        }
+        sc.close();
+    }
+
+}
